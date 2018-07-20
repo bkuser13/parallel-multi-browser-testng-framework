97d7837c3c63196eef417c2ad4c9e0334c4737a5
Bolot <bkuser13@gmail.com>
2018-07-20 12:51:21 -0500

    Parallel browser testing passed

--- src/test/java/com/cbt/tests/TestBase.java
+++ src/test/java/com/cbt/tests/TestBase.java
@@ -3,18 +3,21 @@ package com.cbt.tests;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
-
 import com.cbt.utilities.ConfigurationReader;
 import com.cbt.utilities.Driver;
 
 public abstract class TestBase {
 
 	protected WebDriver driver;
-	
+	/*@parameter ---> means that this method will expect an argument
+	 * from the xml file name of the parameter in the xml file is browser
+	 */
 	@Parameters("browser")
 	@BeforeMethod
-	public void setUp(String browser) {
+	//@optional ---> we user this if we don't want to provide parameters in xml all the time
+	public void setUp(@Optional String browser) {
 		driver = Driver.getDriver(browser);
 		driver.get(ConfigurationReader.getProperty("url"));
 	}
